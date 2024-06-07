44d42248bbdfade4249d3200ed41be121f9a08cc
unknown <sujanbhushal2014@gmail.com>
2024-06-07 12:29:11 +0545

    installation of nodemon and example check on postman

--- server/index.js
+++ server/index.js
@@ -2,10 +2,13 @@ const express = require('express')
 const app = express()
 const port = 4000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/me', (req, res) => {
+  res.send({
+    name:"sujan",
+    message:"Hello World"
+  })
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App listening on port ${port}`)
 })
