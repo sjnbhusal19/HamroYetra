11ad8dcfafa77672fa0909d51785bb760c2a3e44
unknown <sujanbhushal2014@gmail.com>
2024-06-23 11:04:30 +0545

    basic setup of redux and added configure store file

---
+++ client/src/redux/reducerSlices/counterSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = { 
+    value: 0, 
+    userName: ''
+}
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState: initialState,
+  reducers: {
+    increment(state) {
+      state.value=state.value+1
+    },
+    decrement(state) {
+      state.value=state.value-1
+    },
+    reset(state){
+        state.value = 0
+    }
+  },
+})
+
+export const { increment, decrement, reset } = counterSlice.actions
+export default counterSlice.reducer
