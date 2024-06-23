a3e5c752589425ae8f81ecef559e91ac336b967f
unknown <sujanbhushal2014@gmail.com>
2024-06-23 11:36:36 +0545

    finishing redux setup and added boxSlice

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import {NextUIProvider} from "@nextui-org/react";
 import { Toaster } from "react-hot-toast";
+import ReduxProvider from "@/redux/ReduxProvider";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,7 +17,9 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
       <NextUIProvider>
-      {children}
+      <ReduxProvider>
+        {children}
+        </ReduxProvider>
       <Toaster
   position="top-center"
   reverseOrder={false}
--- client/src/redux/reducerSlices/userSlice.js
+++ client/src/redux/reducerSlices/userSlice.js
@@ -1,4 +1,8 @@
+import { createSlice } from "@reduxjs/toolkit"
+
 const initialState = {
     token :'',
     isLoggedIn: false,
 }
+
+const userSlice = createSlice
